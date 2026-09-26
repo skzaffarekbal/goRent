c4aabb6598ab9d4e155e61063c0bbc2b2108ace3
Sk Zaffar Ekbal <s.zaffarekbal@gmail.com>
2026-09-26 20:40:16 +0530

    getCarById query and rating virtual implemented

--- server/graphql/resolvers/car.resolvers.ts
+++ server/graphql/resolvers/car.resolvers.ts
@@ -1,13 +1,15 @@
-import { createCar, getAllCars } from '../../controllers/car.controller';
+import { createCar, getAllCars, getCarById } from '../../controllers/car.controller';
 import { CarInput } from '../../types/car.types';
 
 export const carResolvers = {
   Query: {
-    getAllCars: async (parent: any, args: any, context: any) => getAllCars(),
+    getAllCars: async (parent: any, args: any, context: any) => await getAllCars(),
+    getCarById: async (parent: any, { carId }: { carId: string }, context: any) =>
+      await getCarById(carId),
   },
   Mutation: {
     createCar: async (_: any, { carInput }: { carInput: CarInput }, context: any) => {
-      return createCar(carInput);
+      return await createCar(carInput);
     },
   },
 };
--- server/models/car.model.ts
+++ server/models/car.model.ts
@@ -89,5 +89,12 @@ const carSchema = new mongoose.Schema(
   },
 );
 
+carSchema.virtual('ratings').get(function () {
+  return {
+    value: 5,
+    count: 10,
+  };
+});
+
 const Car = mongoose.model('Car', carSchema);
 export default Car;
